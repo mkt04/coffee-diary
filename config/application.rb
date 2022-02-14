998f9680a009e78305d75b3c2d6074b5adf59323
mkt04 <makoto.okamura.him@gmail.com>
2022-02-14 22:03:44 +0900

    i18nを導入

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ Bundler.require(*Rails.groups)
 
 module CoffeeDiary
   class Application < Rails::Application
+    config.i18n.default_locale = :ja
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
