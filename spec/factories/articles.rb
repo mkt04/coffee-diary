b04e9bdff0a6423f14fb23a5aea1274756bab2db
mkt04 <makoto.okamura.him@gmail.com>
2022-03-15 18:15:18 +0900

    検索機能のrequest specを実装

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :article do
-    date { "2022-01-03 17:44:42" }
-    shop_name { "MyString" }
-    bean_name { "MyString" }
-    producing_area { "MyString" }
+    date { "2022-01-01 00:00:00" }
+    shop_name { "my shop" }
+    bean_name { "my bean" }
+    producing_area { "my producing area" }
     particle_size { 1.5 }
-    memo { "MyText" }
+    memo { "my memo" }
     photos { nil }
     want_to_again { false }
   end
